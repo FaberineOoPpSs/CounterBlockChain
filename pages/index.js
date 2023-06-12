e6289689d628b696a0a1e45ebdb2685b58e7c662
faberine <yashaswi.aryan99@gmail.com>
2023-06-12 11:52:12 +0530

    Fixed issues
    The new etherjs is not working correctly so had to revert back to
    ^5.7.0 because of this had to make changes function callings. Also
    removed app folder which was clashing with index.js in pages folder

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { getContract } from "../ethereum";
+import Counter from "../artifacts/contracts/Counter.sol/Counter.json"
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+  const [contract, setContract] = useState(null);
+
+  useEffect(() => {
+    async function initContract() {
+      const contract = getContract(
+        //"0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        Counter.abi,
+        0 // Use the first account as the signer
+      );
+      setContract(contract);
+      const initialCount = await contract.getCount();
+      setCount(initialCount.toNumber());
+    }
+    initContract();
+  }, []);
+
+  async function increment() {
+    if (!contract) return;
+    const tx = await contract.increment();
+    await tx.wait();
+    const updatedCount = await contract.getCount();
+    setCount(updatedCount.toNumber());
+  }
+
+  return (
+    <div style={{ textAlign: 'center'}}>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
